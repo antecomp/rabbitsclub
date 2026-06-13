4f0b399e9252e6a81e7571c91de6cb8c1008af3c
= <60552584+antecomp@users.noreply.github.com>
2026-06-12 20:50:03 -0600

    Link FE to ws roughly.

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,5 +1,6 @@
-import { createResource, Index } from "solid-js"
+import { createResource, createSignal, For, Index, onCleanup, onMount } from "solid-js"
 import { BE } from "./api"
+import type { Treaty } from "@elysiajs/eden";
 
 function App() {
   const [health] = createResource(async () => {
@@ -7,19 +8,48 @@ function App() {
     return data
   });
 
-  const [recentMessages] = createResource(async () => {
-    const {data} = await BE.messages.get();
+  const [messages, { mutate }] = createResource(async () => {
+    const { data } = await BE.messages.get();
     return data;
   });
 
+  // Good enough for now
+  const [username, setUsername] = createSignal("");
+  const [content, setContent] = createSignal("");
+
+  const sub = BE.ws.subscribe();
+
+  onMount(() => {
+
+    sub.on("message", ({ data }) => {
+      mutate(prev => [...(prev ?? []), data]);
+    });
+
+    onCleanup(() => sub.close());
+  });
+
+  const send = (e: SubmitEvent | MouseEvent | undefined) => {
+    e?.preventDefault();
+    if (!username() || !content()) return;
+    sub.send({ username: username(), content: content() });
+    setContent("");
+  }
+
   return (
     <main>
-      <h1>Chat App</h1>
-      <p>Backend status: {health()?.status}</p>
-      <hr />
-      <Index each={recentMessages()}>
-        {(message) => <p>{message().username}: {message().content}</p>}
-      </Index>
+      <section>{health()?.status}</section>
+      <For each={messages()}>
+        {msg => (
+          <div>
+            <strong>{msg.username}</strong>: {msg.content}
+          </div>
+        )}
+      </For>
+      <form onsubmit={send}>
+        <input value={username()} onInput={e => setUsername(e.target.value)} placeholder="Username" />
+        <input value={content()} onInput={e => setContent(e.target.value)} placeholder="Message" />
+        <button onClick={send}>Send</button>
+      </form>
     </main>
   )
 }
--- apps/server/src/routes/chat.ts
+++ apps/server/src/routes/chat.ts
@@ -9,15 +9,21 @@ export const chatRoutes = new Elysia()
     .ws("/ws", {
         // used to validate message shape
         body: SentMessageSchema,
+        response: MessageSchema,
         open(ws) {
             ws.subscribe("chat")
         },
         message(ws, message) {
             const saved = actions.insertMessage(message.username, message.content);
+            if(!saved) {
+                console.error("Unable to post message to DB");
+                return;
+            }
             ws.publish("chat", saved); // echos to other subscribers.
             ws.send(saved); // echos back to sender.
         },
         close(ws) {
+            console.log("client disconnected:", ws.id);
             ws.unsubscribe("chat")
         }
     });
