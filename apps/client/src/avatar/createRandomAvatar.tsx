759b2b54848bc68e762c248e66549c09a2c173fd
antecomp <60552584+antecomp@users.noreply.github.com>
2026-06-28 14:53:49 -0600

    asdf

--- apps/client/src/avatar/createRandomAvatar.tsx
+++ apps/client/src/avatar/createRandomAvatar.tsx
@@ -1,14 +1,19 @@
 import { clampedHeadVariant, heads, eyeVariants } from "@/avatar/avatar.assets";
 import { createDefaultAvatar } from "@/avatar/avatar.const";
 import { AvatarData } from "@/avatar/avatar.types";
+import getRandomInt from "@/util/getRandomInt";
 import pickRandom from "@/util/pickRandom";
+
+// const EYE_OFFSET_RANGE = 10;
 
 /** Creates a valid randomized avatar state using the available asset variants. */
 export function createRandomAvatar(): Readonly<AvatarData> {
     return {
         ...createDefaultAvatar(),
-        head: clampedHeadVariant(Math.floor(Math.random() * heads.length)),
+        head: clampedHeadVariant(getRandomInt(heads.length)),
         leftEye: pickRandom(eyeVariants),
         rightEye: pickRandom(eyeVariants),
+        // rightEyeOffset: {x: getRandomInt(EYE_OFFSET_RANGE), y: getRandomInt(EYE_OFFSET_RANGE)},
+        // leftEyeOffset: {x: getRandomInt(EYE_OFFSET_RANGE), y: getRandomInt(EYE_OFFSET_RANGE)}
     };
 }
