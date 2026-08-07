3c245cecff5e471d60517a420cd27df99b8757a2
antecomp <60552584+antecomp@users.noreply.github.com>
2026-08-06 22:00:33 -0600

    admin to fetch any users permissions

--- apps/server/src/routes/admin.ts
+++ apps/server/src/routes/admin.ts
@@ -1,11 +1,45 @@
 import Elysia from "elysia";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { UserPermissionsSchema } from "~/schemas/moderation.schema";
+import { UpdateUserPermissionsSchema } from "~/schemas/moderation.schema";
 import { actions } from "~/db/actions";
 import { ErrorSchema } from "~/schemas/generic.schema";
+import { mapObject } from "~/util/mapObject";
+import { createUserPermissions } from "./moderation";
 
 export const adminRoutes = new Elysia({ prefix: '/admin' })
     .use(authMiddleware)
+    .get('/users/:id/permissions', ({ params, status }) => {
+        const targetid = Number(params.id);
+
+        if (!targetid)
+            return status(400, { message: "Invalid user id" });
+
+        const targetUser = actions.users.getUserById(targetid)
+
+        if (!targetUser)
+            return status(404, { message: "User not found" });
+
+        const dbPermissions =
+            actions.moderation.getUserPermissions(targetid);
+
+        const { user_id: _userId, ...permissions } =
+            dbPermissions ?? {
+                user_id: targetid,
+                ...createUserPermissions()
+            };
+
+        return mapObject(
+            permissions,
+            permission => targetUser.is_admin || permission
+        );
+    }, {
+        useAdmin: true,
+        response: {
+            400: ErrorSchema,
+            404: ErrorSchema,
+            500: ErrorSchema
+        }
+    })
     .patch("/users/:id/permissions", ({ params, body: permissions, status }) => {
         const targetid = Number(params.id);
 
@@ -23,7 +57,7 @@ export const adminRoutes = new Elysia({ prefix: '/admin' })
         return storedPermissions;
     }, {
         useAdmin: true,
-        body: UserPermissionsSchema,
+        body: UpdateUserPermissionsSchema,
         response: {
             400: ErrorSchema,
             404: ErrorSchema,
