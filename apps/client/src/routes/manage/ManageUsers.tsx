da4563b879602d6b0301954e0bbd3b4031d39ca3
antecomp <60552584+antecomp@users.noreply.github.com>
2026-08-10 21:59:49 -0600

    Draft user list table in ManageUser

--- apps/client/src/routes/manage/ManageUsers.tsx
+++ apps/client/src/routes/manage/ManageUsers.tsx
@@ -1,13 +1,59 @@
+import { api } from "@/api/backend";
 import Footer from "@/components/Footer";
 import Link from "@/components/Link";
 import usePermissionGuard from "@/hooks/usePermissionGuard";
-import { AuthForm, Divider, Subtitle, Title } from "@/styled/shared.styles";
-import { Show } from "solid-js";
+import { AuthForm, Divider, Subtitle, ThinDivider, Title } from "@/styled/shared.styles";
+import { createResource, For, Show } from "solid-js";
+import { styled } from "solid-styled-components";
+
+const UserSelectionTable = styled('div')`
+    width: 100%;
+    height: 125px;
+    overflow-y: auto;
+    display: block;
+    scrollbar-width: none;
+`
+
+const UserSelectionRowContainer = styled('div')`
+    width: 100%;
+    gap: 5px;
+    display: flex;
+    margin-bottom: 2px;
+
+    span {
+        background: lightgray;
+        color: black;
+        padding: 2px;
+    }
+`
+
+const UserSelectionRowId = styled('span')`
+    width: 3.5ch;
+    text-align: right;
+`
+
+const UserSelectionRowUsername = styled('span')`
+    flex-grow: 1;
+`
+
+function UserSelectionRow(
+    // lmao
+    props: Exclude<Awaited<ReturnType<typeof api.moderation.users.get>>['data'], null>[number]
+) {
+    return (
+        <UserSelectionRowContainer>
+            <UserSelectionRowId>{props.id}</UserSelectionRowId>
+            <UserSelectionRowUsername>{props.username}</UserSelectionRowUsername>
+        </UserSelectionRowContainer>
+    )
+}
 
 export default function ManageUsers() {
     const canAccess = usePermissionGuard('can_ban_users', {
         redirectTo: '/manage'
     });
+
+    const [users, refetch] = createResource(() => api.moderation.users.get().then(({ data }) => data ?? null))
 
     return (
         <Show when={canAccess()}>
@@ -15,7 +61,14 @@ export default function ManageUsers() {
             <Subtitle>User management</Subtitle>
             <Divider />
             <AuthForm>
-                placeholder
+                <UserSelectionTable>
+                    <Show when={users()}>
+                        <For each={users() ?? []}>
+                            {user => <UserSelectionRow {...user} />}
+                        </For>
+                    </Show>
+                </UserSelectionTable>
+                <ThinDivider />
                 <Link href="/manage">[ BACK ]</Link>
             </AuthForm>
             <Footer>Select user to manage.</Footer>
