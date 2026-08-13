3cb80c9ef2162cc4d5bec8bfbcca895d43306433
antecomp <60552584+antecomp@users.noreply.github.com>
2026-08-12 21:18:35 -0600

    manage user avatar & styling

---
+++ apps/client/src/routes/manage/ManageUser.styles.ts
@@ -0,0 +1,53 @@
+import cbr from '@/assets/ui/c_br.png?url&no-inline';
+import { styled } from "solid-styled-components";
+
+export const ManageUserGrid = styled('div')`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    align-items: center;
+    gap: 10px;
+    height: 200px;
+`
+
+export const ManageUserMenu = styled('div')`
+    overflow: auto;
+`
+
+export const AvatarContainer = styled('div')`
+    height: 100%;
+    aspect-ratio: 1;
+    position: relative;    
+
+    canvas {
+        width: 100%;
+        overflow: hidden;
+        height: 100%;
+        object-fit: contain;
+
+        background: url(${cbr});
+
+        --bevel: 20px;
+
+        clip-path: polygon(
+            var(--bevel) 0,
+            calc(100% - var(--bevel)) 0,
+            100% var(--bevel),
+            100% calc(100% - var(--bevel)),
+            calc(100% - var(--bevel)) 100%,
+            var(--bevel) 100%,
+            0 calc(100% - var(--bevel)),
+            0 var(--bevel)
+        );
+    }
+
+    h3 {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+        background-color: #ffffff;
+        padding: 2px;
+
+        clip-path: polygon(100% 0, 100% calc(100% - 10px), calc(100% - 10px) 100%, 0 100%, 0 0);
+    }
+`
--- apps/client/src/routes/manage/ManageUser.tsx
+++ apps/client/src/routes/manage/ManageUser.tsx
@@ -1,9 +1,15 @@
 import { api } from "@/api/backend";
+import { createDefaultAvatar, toAvatarData } from "@/avatar/avatar.const";
+import { AvatarData } from "@/avatar/avatar.types";
+import { AvatarCanvas } from "@/avatar/AvatarCanvas";
 import usePermissionGuard from "@/hooks/usePermissionGuard";
 import { AuthForm, Divider, Subtitle, Title } from "@/styled/shared.styles";
 import { useParams } from "@solidjs/router";
 import { createResource, Show, Suspense } from "solid-js";
 import { type ModerationUser } from "~/schemas/moderation.schema";
+import { AvatarContainer, ManageUserGrid, ManageUserMenu } from "./ManageUser.styles";
+import Footer from "@/components/Footer";
+import Link from "@/components/Link";
 
 export default function ManageUser() {
     const canAccess = usePermissionGuard('can_ban_users', {
@@ -12,15 +18,26 @@ export default function ManageUser() {
 
     const params = useParams<{ id: string }>();
 
-    const [selectedUser] = createResource<ModerationUser | null>(
-        () => {
+    const [selectedUser] = createResource<ModerationUser & { avatar: AvatarData } | null>(
+        async () => {
             const id = Number(params.id);
             if (!id) return null;
-            return api.moderation.user({ id: id })
+            const main = await api.moderation.user({ id: id })
                 .get()
                 .then(({ data }) => data ?? null);
+            if (!main) return null;
+
+            // todo: replace this with a get by id when we change the support there
+            const avatar = await api.profile({ username: main.username })
+                .get()
+                .then(({ data }) => toAvatarData(data) ?? createDefaultAvatar())
+
+            return { ...main, avatar };
         }
-    )
+    );
+
+
+
 
     return (
         <Show when={canAccess()}>
@@ -28,10 +45,28 @@ export default function ManageUser() {
             <Subtitle>User management</Subtitle>
             <Divider />
             <Suspense fallback={<div>Loading user data...</div>}>
-                <AuthForm>
-                    {JSON.stringify(selectedUser())}
-                </AuthForm>
+                <Show when={selectedUser()} fallback={
+                    <AuthForm>
+                        <div>user not found</div>
+                        <Link href={'/manage/users'}>[ BACK ]</Link>
+                    </AuthForm>
+                }>
+                    <AuthForm>
+                        <ManageUserGrid>
+                            <AvatarContainer>
+                                <h3>{selectedUser()!.id}: {selectedUser()?.username}</h3>
+                                <AvatarCanvas state={selectedUser()!.avatar} />
+                            </AvatarContainer>
+                            <ManageUserMenu>
+                                {/* {JSON.stringify(selectedUser())} */}
+                                [ OPTIONS WILL GO HERE ] <br />
+                                <Link href={'/manage/users'}>[ BACK ]</Link>
+                            </ManageUserMenu>
+                        </ManageUserGrid>
+                    </AuthForm>
+                    <Footer>Their fate is in your hands.</Footer>
+                </Show>
             </Suspense>
-        </Show>
+        </Show >
     );
 }
